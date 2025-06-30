316396db46239042a0e154b161b6169001cba8f0
Ishwor karki <ishworkarkij11@gmail.com>
2025-06-30 11:21:10 +1000

    local to production

--- inngest/client.js
+++ inngest/client.js
@@ -1,3 +1,7 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "ticketing-system" });
+export const inngest = new Inngest({
+  id: "ticketing-system",
+  eventKey:
+    "E8MMgGlOeWDy-0Vvux-ZvYo-eoDHhfj8XCZysZ3bx4RzbHGULeKhctRAFcZE__QsRxUpT-u6xQ4zBoNMJQy6Pw",
+});
